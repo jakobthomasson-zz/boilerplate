e80ff38d1034a74248807136913449678febb2f1
jakobthomasson <jakob.thomasson@jayway.com>
2018-09-25 13:39:08 +0200

    add htmlwebpackplugin and htmlloader

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 
 import * as React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
+import App from './App';
 
 render(
   <App message="World" />,
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader'
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    })
+  ]
+
+};
