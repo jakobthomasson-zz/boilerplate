386e95968844675ddb9fbd35da05ab682b8806fc
jakobthomasson <jakob.thomasson@jayway.com>
2018-09-27 15:20:06 +0200

    add Theme type to default props

--- src/components/UI/Button/index.tsx
+++ src/components/UI/Button/index.tsx
@@ -1,10 +1,20 @@
 import React, { MouseEvent, SFC } from 'react';
+import styled from 'styled-components';
 import { color } from 'styles';
 import { withDefaultProps } from 'components/hoc';
 
+const BaseButton = styled.button``;
+
+const PrimaryButton = styled(BaseButton)``;
+const SecondaryButton = styled(BaseButton)``;
+
 // defaultProps are reflected and marked as optional within our type definition but stays required within implementation !
+
+type Theme = 'primary' | 'secondary';
+const defaultTheme: Theme = 'primary';
+
 const defaultProps = {
-  color: color.primary,
+  theme: defaultTheme,
 };
 
 type DefaultProps = typeof defaultProps;
@@ -14,8 +24,8 @@ type Props = {
 } & DefaultProps;
 
 // SFC is just an alias of StatelseeComponent<P> and has pre-defined children and other things (defaultProps, displayName...)
-const Button: SFC<Props> = ({ onClick: handleClick, children, color }) => (
-  <button style={{ color }} onClick={handleClick}>{children}</button>
+const Button: SFC<Props> = ({ onClick: handleClick, children, theme }) => (
+  <button onClick={handleClick}>{children}</button>
 );
 
 export default withDefaultProps(defaultProps, Button);
